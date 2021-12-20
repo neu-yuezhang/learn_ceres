679f3650ea40b209092e6e9c3c027cd3fe122670
Lv-yuezhang <748237371@qq.com>
2021-12-20 19:03:36 +0800

    init

--- test_ceres0.cpp
+++ test_ceres0.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 struct Curve_Factor
 {
-        Curve_Factor(double x, double y) : _x(x) , _y(y){}
+        Curve_Factor(double x, double y) : _x(x), _y(y) {}
         template <typename T>
         //操作符()是一个模板方法，返回值为bool型，接受参数依次为待优化变量和残差变量
-        bool operator() (const T* const abc, T* residual) const
+        bool operator()(const T *const abc, T *residual) const
         {
-                residual[0] = T(_y) - ceres::exp(abc[0] * T(_x) * T(_x) + abc[1] * T(_x) + abc[2]);
+                residual[0] = T(_y) - (abc[0] * T(_x) * T(_x) + abc[1] * T(_x) + abc[2]);
                 return true;
         }
         const double _x, _y;
@@ -20,8 +20,9 @@ struct Curve_Factor
 
 int main(int, char **)
 {
-        double a = 1.0, b = 2.0, c = 1.0;
-        int N = 200;
+        int kk = 0;
+        double a = 1.0, b = 10.0, c = 1.5;
+        int N = 1000;
         double w_sigma = 1.0;
         cv::RNG rng;
         double abc[3] = {0, 0, 0};
@@ -31,19 +32,17 @@ int main(int, char **)
         {
                 double x = i / 100.0;
                 x_data.push_back(x);
-                y_data.push_back(exp(a * x * x + b * x + c) + rng.gaussian(w_sigma));
-                //cout << x_data[i] << "   " << y_data[i] << endl;
+                y_data.push_back(a * x * x + b * x + c + rng.gaussian(w_sigma));
         }
 
         //构造最小二乘问题
         ceres::Problem problem;
-        for(int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++)
         {
                 problem.AddResidualBlock(
-                        new ceres::AutoDiffCostFunction<Curve_Factor, 1, 3>(new Curve_Factor(x_data[i], y_data[i])),
-                        nullptr,
-                        abc
-                );
+                    new ceres::AutoDiffCostFunction<Curve_Factor, 1, 3>(new Curve_Factor(x_data[i], y_data[i])),
+                    nullptr,
+                    abc);
         };
 
         //配置求解器
@@ -55,6 +54,6 @@ int main(int, char **)
         ceres::Solve(options, &problem, &summary);
 
         cout << summary.BriefReport() << endl;
-        for(auto a:abc)
+        for (auto a : abc)
                 cout << a << " " << endl;
 }
